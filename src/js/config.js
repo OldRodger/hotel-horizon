d7016bccab6d588ff98e3424a685c78d42215c4a
oldroger <celexspencer007@gmail.com>
2022-05-13 10:44:26 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,21 @@
+export const API_URL = "https://hotel-horizon.herokuapp.com/api";
+export const TIME_OUT_SEC = 15;
+export const API_POST_CONFIG = {
+    method: 'POST',
+    headers:{
+        'Authorization' : `Basic ${btoa('admin:admin')}`,
+        'Content-Type': 'application/json; charset=utf-8',
+    }, 
+    body:''
+}
+
+export const API_GET_CONFIG = {
+    method: 'GET',
+    headers:{
+        Accept: 'application/json;',
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization' : `Basic ${btoa('admin:admin')}`,
+    }
+}
+
+console.log('Hello');
